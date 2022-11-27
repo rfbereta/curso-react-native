e4298a23654a2d4d8459f1add1f9d6581166bab0
Rodrigo Bereta <rodrigobereta@outlook.com.br>
2022-11-27 17:46:45 -0300

    Projeto com as libs atualizadas e swipe funcionando no android

---
+++ 2023/tasksOffline2023/index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+ import {AppRegistry} from 'react-native';
+ import TaskList from './src/screens/TaskList';
+ import {name as appName} from './app.json';
+ 
+ AppRegistry.registerComponent(appName, () => TaskList);
+ 
